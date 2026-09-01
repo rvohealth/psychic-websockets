e7fdf4dd1707c64fe9ce4fb58dce008567d752e4
avocadojesus <fgarbutt@rvohealth.com>
2026-09-01 12:32:42 -0700

    remove dead env var
    related https://rvohealth.atlassian.net/browse/WLOS-683

--- src/helpers/EnvInternal.ts
+++ src/helpers/EnvInternal.ts
@@ -3,7 +3,7 @@ import { Env } from '@rvoh/dream'
 const EnvInternal = new Env<{
   string: 'NODE_ENV' | 'PSYCHIC_CORE_DEVELOPMENT'
   // integer: 'PORT'
-  boolean: 'DEBUG' | 'PSYCHIC_CORE_DEVELOPMENT' | 'PSYCHIC_DANGEROUSLY_PERMIT_WS_EXCEPTIONS'
+  boolean: 'DEBUG' | 'PSYCHIC_CORE_DEVELOPMENT'
 }>()
 
 export default EnvInternal
